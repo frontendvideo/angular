e857c14e2b159ff1ed7c7e64757f05ef7cca96c5
Kevin Chisholm <kevinchisholm@Kevins-MacBook-Pro.local>
2017-03-11 10:22:42 -0500

    Add disabled-attribute

---
+++ templates-and-data/disabled-attribute/www/src/home/home.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'home',
+  templateUrl: 'src/home/home.html'
+})
+export class HomeComponent {
+  contentHighlighted: boolean = false;
+  disableHighlighting: boolean = false;
+}
